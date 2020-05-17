63aa6e192e8dda1a2a130c22cb2885d9a3572454
alexaspalato <alexaspalato@gmail.com>
2020-05-17 09:41:23 +0200

    scrollmagic ssr

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -5,3 +5,18 @@ exports.createPages = async ({ actions, graphql }) => {
   await createProjects({ actions, graphql })
   await createServices({ actions, graphql })
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /ScrollMagicPluginGsap/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
--- site/src/components/Parallax.js
+++ site/src/components/Parallax.js
@@ -1,11 +1,25 @@
 /** @jsx jsx */
 import { jsx, Container, Flex, Box } from 'theme-ui'
+import React, { useRef } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import BgImage from './images/BgImage'
-import { Controller, Scene } from 'react-scrollmagic'
-import { Tween, Timeline } from 'react-gsap'
+import * as ScrollMagic from 'scrollmagic-with-ssr'
+import { TweenMax, TimelineMax } from 'react-gsap'
+import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap'
+
+ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax)
 
 const Parallax = (props) => {
+  let overlayRef = useRef(null),
+    textOneRef = useRef(null),
+    textTwoRef = useRef(null),
+    overlayEl = overlayRef.current,
+    textOneEl = textOneRef.current,
+    textTwoEl = textTwoRef.current
+
+  let controller = new ScrollMagic.Controller()
+  // const bgTween = new TweenMax.from(overlayEl,)
+
   const data = useStaticQuery(graphql`
     query {
       wp {
@@ -23,59 +37,19 @@ const Parallax = (props) => {
   const { heroImage } = data.wp.themeOptions.homeImage
 
   return (
-    <Controller>
-      <Scene
-        duration="100%"
-        triggerHook="onLeave"
-        pin={{ pushFollowers: false, spacerClass: 'homeSpacer' }}
-        enabled={true}
-        indicators={true}
-      >
-        {(progress) => (
-          <Box sx={{ ...styles }} {...props}>
-            <BgImage
-              img={heroImage}
-              className="bgImage"
-              sx={{ minHeight: 800 }}
-            >
-              <Timeline totalProgress={progress} paused>
-                <Tween
-                  from={{ css: { background: 'rgba(0,0,0,.6)' } }}
-                  to={{ css: { background: 'rgba(0,0,0,.8)' } }}
-                >
-                  <Flex className="overlay">
-                    <Timeline
-                      target={
-                        <h1 className="textOne">
-                          this is your <span>Design Lab</span>
-                        </h1>
-                      }
-                    >
-                      <Tween from={{ y: 200 }} to={{ y: 0 }} />
-                      <Tween from={{ autoAlpha: 1 }} to={{ autoAlpha: 0 }} />
-                    </Timeline>
-                    <Timeline
-                      target={
-                        <h2 className="textTwo">
-                          At The Falcon Lab, We strive to intimately understand
-                          your brand so we are able to fluidly execute your
-                          vision.{' '}
-                        </h2>
-                      }
-                    >
-                      <Tween
-                        from={{ autoAlpha: 0, y: 100 }}
-                        to={{ autoAlpha: 1, y: -100 }}
-                      />
-                    </Timeline>
-                  </Flex>
-                </Tween>
-              </Timeline>
-            </BgImage>
-          </Box>
-        )}
-      </Scene>
-    </Controller>
+    <Box sx={{ ...styles }} {...props}>
+      <BgImage img={heroImage} className="bgImage" sx={{ minHeight: 800 }}>
+        <Flex ref={overlayRef} className="overlay">
+          <h1 ref={textOneRef} className="textOne">
+            this is your <span>Design Lab</span>
+          </h1>
+          <h2 ref={textTwoRef} className="textTwo">
+            At The Falcon Lab, We strive to intimately understand your brand so
+            we are able to fluidly execute your vision.
+          </h2>
+        </Flex>
+      </BgImage>
+    </Box>
   )
 }
 
