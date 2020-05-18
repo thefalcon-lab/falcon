ca71dc28a07b5a596b21d11d4225f2f6409a1fb7
alexaspalato <alexaspalato@gmail.com>
2020-05-18 21:12:04 +0200

    svg

--- site/src/components/BrandAnimation.js
+++ site/src/components/BrandAnimation.js
@@ -1,11 +1,17 @@
 /** @jsx jsx */
 import { jsx, Container, Flex, Box } from 'theme-ui'
-import { Brand } from '../svg/all-icons'
+import { Brand } from './Brand'
 
 const BrandAnimation = () => {
   return (
     <Box sx={{ bg: 'black' }}>
-      <Brand sx={{}} />
+      <Brand
+        sx={{
+          '.st0': { color: 'white' },
+          '.st1': { color: 'primary' },
+          '.st2': { color: '#4D4D4D' },
+        }}
+      />
     </Box>
   )
 }
