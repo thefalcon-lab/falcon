7047a4de7449904d9ec8b0df154655f624ad4a42
alexaspalato <alexaspalato@gmail.com>
2020-05-25 08:04:41 +0200

    favicon

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -68,5 +68,23 @@ module.exports = {
         username: `thefalconlab`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'falconlab',
+        short_name: 'falconlab',
+        start_url: '/',
+        background_color: '#000',
+        theme_color: '#D63F18',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/logo.svg', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
