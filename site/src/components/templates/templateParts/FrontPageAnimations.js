b07a7fe8ae5e3277b5aa417ddaae0a0b0ca4ff21
alexaspalato <alexaspalato@gmail.com>
2020-06-04 19:45:15 +0200

    animations

--- site/src/components/templates/templateParts/FrontPageAnimations.js
+++ site/src/components/templates/templateParts/FrontPageAnimations.js
@@ -3,17 +3,17 @@ import { jsx, Container, Flex, Box } from 'theme-ui'
 import React from 'react'
 // import BrandAnimation from '../../BrandAnimation'
 import Parallax from '../../Parallax'
+import ParsedContent from '../../../utils/ParsedContent'
 
 import brandAnimation from '../../../../static/brandAnimation.html'
 
 export const FrontPageAnimations = ({ page, ...props }) => {
   return (
     <Box {...props}>
-      {/* <div id="brand">
-        <ParsedContent content={brandAnimation} />
-      </div> */}
-
       <Parallax />
+      <div id="brand">
+        <ParsedContent content={brandAnimation} />
+      </div>
     </Box>
   )
 }
