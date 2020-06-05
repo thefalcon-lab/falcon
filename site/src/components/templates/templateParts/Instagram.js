2c78e9666f8bc9344adb903a6de8309d1136e4d8
alexaspalato <alexaspalato@gmail.com>
2020-06-05 08:34:46 +0200

    insta conditiional

--- site/src/components/templates/templateParts/Instagram.js
+++ site/src/components/templates/templateParts/Instagram.js
@@ -4,41 +4,41 @@ import { useStaticQuery, graphql } from 'gatsby'
 import BgImage from '../../images/BgImage'
 
 export const Instagram = () => {
-  // const data = useStaticQuery(graphql`
-  //   query MyQuery {
-  //     allInstaNode(limit: 8) {
-  //       nodes {
-  //         localFile {
-  //           publicURL
-  //           childImageSharp {
-  //             fluid(maxWidth: 600, quality: 80) {
-  //               ...GatsbyImageSharpFluid_noBase64
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allInstaNode(limit: 8) {
+        nodes {
+          localFile {
+            publicURL
+            childImageSharp {
+              fluid(maxWidth: 600, quality: 80) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
 
-  // const { nodes } = data.allInstaNode
+  const posts = data?.allInstaNode?.nodes
 
   return (
     <Flex id="instagram" sx={{ flexWrap: 'wrap' }}>
-      {/* {nodes &&
-        nodes.map((node, i) => (
+      {posts?.length &&
+        posts.map((post, i) => (
           <Box
             key={i}
             className="itemContainer"
             sx={{ width: ['100%', '50%', '25%'], p: 1 }}
           >
             <BgImage
-              img={node}
+              img={post}
               className="item"
               sx={{ minHeight: 385 }}
             ></BgImage>
           </Box>
-        ))} */}
+        ))}
     </Flex>
   )
 }
