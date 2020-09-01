dcf0873d06bf1eaacde2ea1105ab0407857cd71b
alexaspalato <alexaspalato@gmail.com>
2020-09-01 08:42:38 +0200

    back projects button

--- site/src/components/project/ProjectHeader.js
+++ site/src/components/project/ProjectHeader.js
@@ -1,10 +1,16 @@
 /** @jsx jsx */
-import { jsx, Box, Container } from 'theme-ui'
+import { jsx, Box, Container, Flex, Button } from 'theme-ui'
+import { Link } from 'gatsby'
 import { Spacer } from '../ui-components'
 
 export const ProjectHeader = ({ title, subtitle, ...props }) => (
   <Container sx={{ maxWidth: 'l' }} {...props}>
-    <h1 className="gsReveal" dangerouslySetInnerHTML={{ __html: title }} />
+    <Flex sx={{ justifyContent: 'space-between' }}>
+      <h1 className="gsReveal" dangerouslySetInnerHTML={{ __html: title }} />
+      <Link to="/our-work" sx={{ '.our-work-page &': { display: 'none' } }}>
+        <Button>View all Projects</Button>
+      </Link>
+    </Flex>
     <Spacer className="gsReveal" />
     {subtitle && (
       <Box
