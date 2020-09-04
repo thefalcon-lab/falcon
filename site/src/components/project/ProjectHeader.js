e141288a01045dfa33a8071e7c9f669ecb48a9c9
alexaspalato <alexaspalato@gmail.com>
2020-09-04 16:50:47 +0200

    hubspot

--- site/src/components/project/ProjectHeader.js
+++ site/src/components/project/ProjectHeader.js
@@ -2,26 +2,30 @@
 import { jsx, Box, Container, Flex, Button } from 'theme-ui'
 import { Link } from 'gatsby'
 import { Spacer } from '../ui-components'
+import { HubspotLogo } from '../Icons'
 
-export const ProjectHeader = ({ title, subtitle, ...props }) => (
-  <Container sx={{ maxWidth: 'l' }} {...props}>
-    <Flex sx={{ justifyContent: 'space-between' }}>
+export const ProjectHeader = ({ title, subtitle, ...props }) => {
+  return (
+    <Container sx={{ maxWidth: 'l' }} {...props}>
       <h1 className="gsReveal" dangerouslySetInnerHTML={{ __html: title }} />
-    </Flex>
-    <Spacer
-      className="gsReveal"
-      sx={{ '.projectSingle &': { mb: [50, 80] } }}
-    />
-    {subtitle && (
-      <Box
-        className="subtitle gsReveal"
-        sx={{
-          variant: 'text.subtitle',
-          maxWidth: 640,
-          '.projectSingle &': { display: 'none' },
-        }}
-        dangerouslySetInnerHTML={{ __html: subtitle }}
+      <Spacer
+        className="gsReveal"
+        sx={{ '.projectSingle &': { mb: [50, 80] } }}
       />
-    )}
-  </Container>
-)
+      {subtitle && (
+        <Flex sx={{ justifyContent: 'space-between' }}>
+          <Box
+            className="subtitle gsReveal"
+            sx={{
+              variant: 'text.subtitle',
+              maxWidth: 640,
+              '.projectSingle &': { display: 'none' },
+            }}
+            dangerouslySetInnerHTML={{ __html: subtitle }}
+          />
+          {title.toLowerCase() === 'marketing' && <HubspotLogo />}
+        </Flex>
+      )}
+    </Container>
+  )
+}
